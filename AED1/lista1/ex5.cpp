9d4944ab7714d0f1e0c95fb4a66098a70b1a9067
Danilo <danilolmd04@gmail.com>
2023-10-06 11:35:30 -0300

    ex5

---
+++ AED1/lista1/ex5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void bin(int num){
+    if(num/2 != 0) bin(num/2);
+    cout << num % 2;
+}
+
+int main(){
+    int qtd, num, c;
+
+    cin >> qtd;
+
+    for(c = 0; c < qtd; c++){
+        cin >> num;
+        bin(num);
+        cout << endl;
+    }   
+
+    return 0;   
+}
